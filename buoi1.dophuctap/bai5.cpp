19c67cb9c47fb26b287b138eeb7d7116eeb72c00
Son Nguyen <son0966056388@gmail.com>
2020-12-05 08:01:39 +0700

    remove empty lines

--- buoi1.dophuctap/bai5.cpp
+++ buoi1.dophuctap/bai5.cpp
@@ -5,26 +5,21 @@ int main() {
     int T;
     cin >> T;
     for (int t=0; t < T; t++) {
-
         int R, k, N;
         cin >> R >> k >> N;
         int g[N];
-
         // R la so luot di cua tau
         // k: tau chi chua dc k nguoi
         // N: co N group
         // g[N]: so luong nguoi moi group
-
         for (int i=0; i<N;i++) {
             cin>> g[i];
         }
 
-
-        
         int cost = 0;
         int bag = 0;
         int times = 0;
-        
+
         for (int z = 0; z< R*N; z++) {
 
             if (times == R) {
@@ -49,9 +44,8 @@ int main() {
         if (k >= total) {
             cost = R*total;
         }
-
+        
         cout <<"Case #" <<t+1<<": "<<cost<<"\n";
-
     }
     return 0;
 }
